4b462e4dbc36c7962a0d74150571ae6943b7b4d1
nitin goyal <inbox.nitin.goyal@gmail.com>
2025-11-05 22:15:45 +0530

    feat: add inital setup

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+
+import { useEffect } from 'react'
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [message, setMessage] = useState('')  
+  useEffect(() => {
+    fetch('http://localhost:4000/api/data')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.message)
+        setMessage(data.message)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error)
+      })
+  }, [])
+
+  return (
+    <>
+      <h1>{message}</h1>
+    </>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";    
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors(
+  {
+    origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://localhost:5174'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}
+));
+app.use(express.json());
+
+// a simple route
+app.get("/api/data", (req, res) => {
+  res.json({ message: "Welcome to Aadat Khata Server." });
+});
+
+app.listen(PORT,"0.0.0.0", () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
